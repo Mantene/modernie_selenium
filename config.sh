41cefe613e471a2c71b011899cd7ba81317c5705
Ásgeir Bjarnason <asgbja@gmail.com>
2016-08-05 17:05:36 +0100

    Update config.sh
    Decrease VM memory as the IE8 machine couldn't handle it. Also remove unused XP memory stuffs..

--- config.sh
+++ config.sh
@@ -14,9 +14,8 @@ else
 fi
 
 vm_path="/home/storystream/modernie_selenium/VMs/"
-vm_mem="3200"
+vm_mem="2615"
 vm_vram="50"
-vm_mem_xp="512"
 deuac_iso="deuac.iso"
 tools_path="/home/storystream/modernie_selenium/Tools/"
 selenium_path="/home/storystream/modernie_selenium/Tools/selenium_conf/"
